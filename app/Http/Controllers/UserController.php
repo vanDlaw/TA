66f6591468989d3253a93168e41df491ec92df13
vanDlaw <ervanguruh@gmail.com>
2017-01-07 00:05:27 +0700

    fc

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        return view('user.profile', ['user' => User::findOrFail($id)]);
+    }
+    public function daftar(Request $req){
+
+      //dd($req->all());
+      $output['task']="Add New User";
+      $output['status']="500";
+      $output['message']="error";
+
+      $imei=$req->input("no");
+      $pin=$this->gen_uid(6);
+
+      $item=User::adduser($imei,$pin);
+
+      if($item != null){
+        $output['status']="200";
+        $output['message']="success";
+        $output['data']=$item->pin;
+      }else{
+
+      }
+
+      return response()->json($output);
+    }
+
+    public function lokasi(Request $req){
+      $output['task']="Add Location";
+      $output['status']="500";
+      $output['message']="error";
+
+      $latitude=$req->input("latitude");
+      $longitude=$req->input("longitude");
+      $pin=$req->input("pin");
+
+
+
+      $item =User::savelocation($pin,$latitude,$longitude);
+
+      $output['status'] = "200";
+      $output['message'] = $item;
+
+      return response()->json($output);
+    }
+    public function getPos(Request $req, $pin){
+      $output['task']="Get Location";
+      $output['status']="500";
+      $output['message']="error";
+      $item =User::getlocation($pin);
+      if ($item == null){
+        $output['status']="400";
+      }else{
+        $output['status']="200";
+        $output['message']='success';
+        $output['data']['pin']=$pin;
+        $output['data']['latitude']=$item->latitude;
+        $output['data']['longitude']=$item->longitude;
+      }
+      return response()->json($output);
+    }
+
+
+    public function gen_uid($l=10){
+    return substr(str_shuffle("123456789ABCDEFGHIJKLMNPQRSTUVWXYZ"), 0, $l);
+    }
+}
---
+++ app/User.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App;
+
+use Exception;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'no', 'mac', 'pin',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    public static function adduser($imei,$pin){
+      try{
+        $item=User::where('imei',$imei)->first();
+        if($item == null){
+          $item = new User();
+          $item->imei = $imei;
+          $item->pin =$pin;
+          $item->save();
+        }
+      }catch(Exception $e){
+        dd($e);
+      }
+      return $item;
+    }
+
+  public static function getlocation($pin){
+    $item=User::where('pin',$pin)->first();
+    return $item;
+  }
+
+
+  public static function savelocation($pin, $lat,$long){
+    $item = User::where('pin',$pin)->first();
+    $item->longitude =$long;
+    $item->latitude = $lat;
+    $item->save();
+
+    return $item;
+  }
+}
